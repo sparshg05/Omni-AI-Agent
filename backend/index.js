c9bcb7b8ff86dfbebdc68e37b086e03c15ba6414
Sparsh Goyal <sparshg05@gmail.com>
2025-10-29 20:36:11 +0530

    update in cors

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://omni-ai-agent-frontend.onrender.com/'],
+    origin: ['http://localhost:3000', 'https://omni-ai-agent-frontend.onrender.com'],
     credentials: true,
 }
 
